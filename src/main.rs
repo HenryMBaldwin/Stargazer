b1b81cc4347ab62bf1538f3cba270c880eda4732
Henry Baldwin <henrymbaldwin@gmail.com>
2023-10-11 15:37:10 -0400

    A couple blocks in a window using dioxus for UI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!")
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        main {
+            style: "display: flex;",
+
+            div {
+                style: "background-color: black; width: 200px; height: 100vh;",
+            },
+
+            div {
+                style: "flex-grow: 1; background-color: grey;",
+            },
+        }
+    })
 }
