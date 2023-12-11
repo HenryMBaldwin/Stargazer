85c8a4e34d4f2290dbf921c71e939f7036d5ed6d
Henry Baldwin <henrymbaldwin@gmail.com>
2023-12-11 17:36:58 -0500

    Made db controller thread safe and other small adjustments.

--- src/server/db_controller.rs
+++ src/server/db_controller.rs
@@ -1,20 +1,24 @@
-use rusqlite::{Connection, Result};
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 use sha2::{Sha256, Digest};
+use anyhow::{Result,Error};
 
 pub struct DBController {
-    conn: Connection,
+    pool: Pool<SqliteConnectionManager>,
 }
 
 impl DBController{
     pub fn new() -> Result<Self> {
-        let conn = Connection::open("db/queries.db")?;
-        self.init()?;
-        Ok(Self{conn})
+        let manager = SqliteConnectionManager::file("db/queries.sqlite");
+        let pool = Pool::new(manager)?;
+        let db_controller = Self { pool };
+        db_controller.init()?;
+        Ok(db_controller)
     }
-
     //creates table if it doesn't exist
     fn init(&self) -> Result<()> {
-        self.conn.execute(
+        let conn = self.pool.get()?;
+        conn.execute(
             "CREATE TABLE IF NOT EXISTS queries (
                 hash TEXT PRIMARY KEY NOT NULL,
                 response TEXT NOT NULL
@@ -25,9 +29,9 @@ impl DBController{
     }
 
     //generates a hash as the primary key for a query
-    fn generate_hash(id: i32, args: &[String]) -> String {
+    fn generate_hash(id: String, args: &Vec<String>) -> String {
         let mut hasher = Sha256::new();
-        hasher.update(id.to_string());
+        hasher.update(id);
         for arg in args {
             hasher.update(arg);
         }
@@ -36,27 +40,30 @@ impl DBController{
     }
 
     //checks if a query exists in the database
-    pub fn query_exists(&self, id: i32, args: &[String]) -> Result<bool> {
+    pub fn query_exists(&self, id: String, args: &Vec<String>) -> Result<bool> {
+        let conn = self.pool.get()?;
         let hash = Self::generate_hash(id, args);
-        let mut stmt = self.conn.prepare("SELECT hash FROM queries WHERE hash = ?1")?;
+        let mut stmt = conn.prepare("SELECT hash FROM queries WHERE hash = ?1")?;
         let mut rows = stmt.query([hash])?;
         Ok(rows.next()?.is_some())
     }
 
-    pub fn get_query(&self, id: i32, args: &[String]) -> Result<String> {
+    pub fn get_query(&self, id: String, args: &Vec<String>) -> Result<String> {
+        let conn = self.pool.get()?;
         let hash = Self::generate_hash(id, args);
-        let mut stmt = self.conn.prepare("SELECT response FROM queries WHERE hash = ?1")?;
+        let mut stmt = conn.prepare("SELECT response FROM queries WHERE hash = ?1")?;
         let mut rows = stmt.query([hash])?;
         let row = rows.next()?.unwrap();
         Ok(row.get(0)?)
     }
 
     //inserts a query with result
-    pub fn insert_query(&self, id: i32, args: &[String], response: &str) -> Result<()> {
+    pub fn insert_query(&self, id: String, args: &Vec<String>, response: &str) -> Result<()> {
+        let conn = self.pool.get()?;
         let hash = Self::generate_hash(id, args);
-        self.conn.execute(
+        conn.execute(
             "INSERT OR REPLACE INTO queries (hash, response) VALUES (?1, ?2)",
-            [hash, response],
+            [hash, String::from(response)],
         )?;
         Ok(())
     }
