17dc111afee77e498f671e42916b36b40051ee04
Henry Baldwin <henrymbaldwin@gmail.com>
2023-12-12 18:15:06 -0500

    supressed non snake case warnings.

--- src/server/json_types.rs
+++ src/server/json_types.rs
@@ -1,7 +1,7 @@
-
 use serde::{Deserialize, Serialize};
 
 //single prompt in "prompts" field for query
+#[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Prompt {
     id: u32,
