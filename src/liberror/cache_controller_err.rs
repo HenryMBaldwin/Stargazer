0928812e84eb8992a85b2ee8b6d2acd7aefc20f2
Henry Baldwin <henrymbaldwin@gmail.com>
2023-12-13 18:02:19 -0500

    Added some more cache controller error types.

--- src/liberror/cache_controller_err.rs
+++ src/liberror/cache_controller_err.rs
@@ -4,4 +4,7 @@ use thiserror::Error;
 pub enum CacheControllerError{
     #[error("Key {0} has unsupported JSON type.")]
     UnsupportedJsonType(String),
+    //generic error for create_table function. Possibly more specific errors will be added later    
+    #[error("Couldn't create table for entry : {0}")]
+    CreateTableError (String),
 }
