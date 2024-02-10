478e519ad4882c58601ab5859484aad48d512f7c
Henry Baldwin <henrymbaldwin@gmail.com>
2024-02-10 15:37:42 -0500

    Added error checking, some functionality for the enter key, auth checking, and some loaders for style.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,106 @@
     import { goto } from '$app/navigation';
     import {invoke} from '@tauri-apps/api/tauri';
     import {StatusCodes} from 'http-status-codes';
+    import {onMount} from 'svelte';
+    import Loader from '$lib/components/Loader.svelte';
+
     let username = '';
     let password = '';
+    let errorMessage = '';
+    /**
+	 * @type {HTMLInputElement}
+	 */
+    let passwordInput;
 
-
+    let isLoading = false;
+    let authChecked = false;
+    onMount(() => {
+        invoke('check_auth').then((res) => {
+            if (res) {
+                goto('/dashboard');
+            }
+            authChecked = true;
+        });
+        
+    });
     async function loginFunction() {
-        console.log('Login function called');
-        let stat = await invoke('login', {username: username, password: password});
-         console.log('Login status: ' + stat);
-        if (stat == StatusCodes.OK) {
-            goto('/dashboard');
-        } else {
-            console.log('Login failed');
+        isLoading = true;
+        errorMessage = '';
+        try {
+            if (username == '') {
+                errorMessage = 'Error: Username cannot be empty.';
+                return;
+            }
+            else if(password == '') {
+                errorMessage = 'Error: Password cannot be empty.';
+                return;
+            }
+            else {
+                let stat = await invoke('login', {username: username, password: password});
+                console.log('Login status: ' + stat);
+                if (stat == StatusCodes.OK) {
+                    goto('/dashboard');
+                } else {
+                    errorMessage = 'Error: Invalid username or password.';
+                }
+            }
+        } finally {
+            isLoading = false;
+        }
+    }
+
+    /**
+	 * @param {{ key: string; }} event
+	 */
+    function handleUsernameKeyDown(event) {
+        if (event.key === 'Enter') {
+            passwordInput.focus(); // Focus on the password input when Enter is pressed
+        }
+    }
+
+    /**
+	 * @param {{ key: string; }} event
+	 */
+    function handlePasswordKeyDown(event) {
+        if (event.key === 'Enter') {
+            loginFunction(); // Trigger the login function when Enter is pressed
         }
     }
 
 </script>
   
 <div class="login-container">
-<div class="image-container">
-    <!-- svelte-ignore a11y-img-redundant-alt -->
-    <img src="src\lib\assets\stargazer_black_vert_transparent.png" alt="Login Image">
-</div>
-<div class="form-container">
-    <input type="text" bind:value={username} placeholder="Username" class="login-input">
-    <input type="password" bind:value={password} placeholder="Password" class="login-input">
-    <button on:click={loginFunction}>Login</button>
-</div>
+    {#if authChecked}
+    <div class="image-container">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="src\lib\assets\stargazer_black_vert_transparent.png" alt="Login Image">
+    </div>
+    <div class="form-container">
+        
+                <p class="error-message">{errorMessage}</p>
+        
+        <input type="text"
+                bind:value={username}
+                placeholder="Username"
+                class="login-input"
+                on:keydown={handleUsernameKeyDown}
+                autofocus>
+
+            <input type="password"
+                bind:value={password}
+                placeholder="Password"
+                class="login-input"
+                on:keydown={handlePasswordKeyDown}
+                bind:this={passwordInput}>
+            {#if isLoading}
+                <Loader />
+            {:else}
+                <button on:click={loginFunction}>Login</button> <!-- Display button otherwise -->
+            {/if}
+    </div>
+    {:else}
+        <Loader />
+    {/if}
 </div>
   
 <style>
@@ -73,5 +146,11 @@
 
     button:hover {
     background-color: #ac8b1f;
+    }
+
+    .error-message {
+        color: red;
+        font-size: medium;
+        margin: 0px 0;
     }
 </style>
