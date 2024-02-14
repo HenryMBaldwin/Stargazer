87c269f02d9979112cc9fde388aa8c40503c2dd6
Henry Baldwin <henrymbaldwin@gmail.com>
2024-02-13 19:07:42 -0500

    Added error page(s) with a very sad cat for unimplemented dashboard pages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+// @ts-nocheck
+
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+    import {onMount} from 'svelte';
+
+    onMount(() => {
+        goto('/');
+    });
+</script>
+
+<h1>{$page.status} {$page.error.message}</h1>
---
+++ src/routes/dashboard/+error.svelte
@@ -0,0 +1,23 @@
+<div class = "page-container">
+    <img src="/assets/sad_cat.png" alt="sad cat">
+    <h1>Uh Oh!</h1>
+    <p>This feature doesn't exist yet. Stargazer is a work in progress!</p>
+</div>
+
+<style>
+    img {
+        width: 100px;
+        max-width: 80%;
+        height: auto;
+        margin: 20px;}
+    .page-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        padding: 20%;
+        height: 100%;
+        margin: 0;
+    }
+</style>
