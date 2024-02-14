aea322005bd5b8d4208ea80b638363187e968758
Henry Baldwin <henrymbaldwin@gmail.com>
2024-02-14 11:28:48 -0500

    Basic functionless system tray and stargazer now stays open in the background.

--- src-tauri/src/gui/gui.rs
+++ src-tauri/src/gui/gui.rs
@@ -1,11 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent};
+use tauri::Manager;
 
 mod pipe_client;
 fn main() {
+  let tray_menu = SystemTrayMenu::new();
+  let system_tray = SystemTray::new()
+    .with_menu(tray_menu);
   tauri::Builder::default()
+    .system_tray(system_tray)
     .invoke_handler(tauri::generate_handler![pipe_client::login, pipe_client::check_auth, pipe_client::check_alive])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .build(tauri::generate_context!())
+    .expect("error while running tauri application")
+    .run(|_app_handle, event| match event {
+      tauri::RunEvent::ExitRequested { api, .. } => {
+        api.prevent_exit();
+      }
+      _ => {}
+    });
 }
 
