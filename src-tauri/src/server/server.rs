c8635e0bb57606b805740d0fcbd3af6154fce6a1
Henry Baldwin <henrymbaldwin@gmail.com>
2024-02-16 21:41:47 -0500

    Changes to do with new cache variable in query request.

--- src-tauri/src/server/server.rs
+++ src-tauri/src/server/server.rs
@@ -177,10 +177,11 @@ async fn handle_request(request: &str, orion_api: Arc<OrionAPI>, cache_controlle
         //Query request
         Ok(RequestType::Query(query_request)) => {
             let query_id: String = query_request.id.clone();
+            let query_cache: bool = query_request.cache.clone();
             let query_args:  Vec<String> = query_request.args.clone();
 
             //check if query is cached
-            if cache_controller.query_exists(query_id.clone(), &query_args).unwrap()  /* && false this disables caching for testinag purposes */ {
+            if cache_controller.query_exists(query_id.clone(), &query_args).unwrap() && query_cache /* && false this disables caching for testinag purposes */ {
                 let result = cache_controller.get_query(query_id.clone(), &query_args).unwrap();
                 let resp = ResponseType::Query(QueryResponse {
                     status: StatusCode::OK.as_u16(),
