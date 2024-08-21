7ca25de2f2857b3690e5cf64c109a135c1e7cbe6
Henry Baldwin <henrymbaldwin@gmail.com>
2024-08-21 11:40:54 -0400

    Main window no longer dies on close, added light system tray context menu to allow show hide and quit.

--- src-tauri/src/gui/gui.rs
+++ src-tauri/src/gui/gui.rs
@@ -1,16 +1,59 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent};
-use tauri::Manager;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent, Manager, WindowEvent};
 
 mod pipe_client;
+
 fn main() {
-  let tray_menu = SystemTrayMenu::new();
+  // Set up system tray menu
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit".to_string());
+  let show = CustomMenuItem::new("show".to_string(), "Show".to_string());
+
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(quit)
+    .add_item(show);
+
   let system_tray = SystemTray::new()
     .with_menu(tray_menu);
+
   tauri::Builder::default()
+    .setup(|app| {
+      let main_window = app.get_window("main").unwrap();
+      let main_window_clone = main_window.clone(); // Clone the window handle
+      
+      // Listen for the close request event and hide the window instead
+      main_window.on_window_event(move |event| {
+        if let WindowEvent::CloseRequested { api, .. } = event {
+          // Prevent the window from closing
+          api.prevent_close();
+          // Hide the window instead
+          main_window_clone.hide().unwrap();
+        }
+      });
+      Ok(())
+    })
     .system_tray(system_tray)
-    .invoke_handler(tauri::generate_handler![pipe_client::login, pipe_client::check_auth, pipe_client::check_alive, pipe_client::get_query_log])
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::MenuItemClick { id, .. } => {
+        match id.as_str() {
+          "quit" => {
+            app.exit(0);
+          }
+          "show" => {
+            if let Some(window) = app.get_window("main") {
+              window.show().expect("Failed to show window");
+            }
+          }
+          _ => {}
+        }
+      },
+      _ => {}
+    })
+    .invoke_handler(tauri::generate_handler![
+      pipe_client::login,
+      pipe_client::check_auth,
+      pipe_client::check_alive,
+      pipe_client::get_query_log
+    ])
     .build(tauri::generate_context!())
     .expect("error while running tauri application")
     .run(|_app_handle, event| match event {
@@ -20,4 +63,3 @@ fn main() {
       _ => {}
     });
 }
-
