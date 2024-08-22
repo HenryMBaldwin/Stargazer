d23b43b48f43f7c54a957627adeaa12ea737dedf
Henry Baldwin <henrymbaldwin@gmail.com>
2024-08-22 17:02:49 -0400

    Removed system tray icon from conf, now there ise only one, fully functional tray icon.

--- src-tauri/src/gui/gui.rs
+++ src-tauri/src/gui/gui.rs
@@ -1,16 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use tauri::{
-  menu::MenuBuilder,
+  menu::{MenuBuilder, MenuItemBuilder},
   tray::{
-    TrayIconBuilder,
-    TrayIconEvent,
-    MouseButton,
-    MouseButtonState,
+    MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent
   },
   Manager,
   WindowEvent
 };
-//CustomMenuItem, SystemTrayMenu, SystemTrayEvent, 
 use stargazer::libinstance::instance::{ClientInstance, generate_id};
 use chrono::Utc;
 mod pipe_client;
@@ -27,14 +23,34 @@ fn main() {
   tauri::Builder::default()
     .manage(client)
     .setup(|app| {
+
+      let hide_item = MenuItemBuilder::with_id("hide", "Hide").build(app)?;
+      let show_item = MenuItemBuilder::with_id("show", "Show").build(app)?;
+      let icon = tauri::image::Image::from_path("icons/icon.ico").unwrap();
       //system tray and menu
       let menu = MenuBuilder::new(app)
-        .hide()
-        .show_all()
+        .items(&[&hide_item, &show_item])
+        .separator()
         .quit()
         .build()?;
       let tray = TrayIconBuilder::new()
+        .icon(icon)
         .menu(&menu)
+        .on_menu_event(move |app, event| match event.id().as_ref() {
+          "hide" => {
+            let webview_window = app.get_webview_window("main").unwrap();
+            let _ = webview_window.hide();
+          },
+          "show" => {
+            let webview_window = app.get_webview_window("main").unwrap();
+            let _ = webview_window.show();
+            if webview_window.is_minimized().unwrap() {
+              let _ = webview_window.unminimize();
+            }
+            let _ = webview_window.set_focus();
+          },
+          _ => {},
+        })
         .on_tray_icon_event(|tray, event| {
           if let TrayIconEvent::Click {
                   button: MouseButton::Left,
@@ -45,6 +61,9 @@ fn main() {
               let app = tray.app_handle();
               if let Some(webview_window) = app.get_webview_window("main") {
               let _ = webview_window.show();
+              if webview_window.is_minimized().unwrap() {
+                let _ = webview_window.unminimize();
+              }
               let _ = webview_window.set_focus();
               }
           }
