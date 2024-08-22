d7ed58653cfe5932e377b618cc2e2ed7cad1fa02
Henry Baldwin <henrymbaldwin@gmail.com>
2024-08-22 12:00:48 -0400

    digit only version string now stored as env var.

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use serde_json;
+
 fn main() {
+  
+  //get version from tauri config
+  let config_str = fs::read_to_string("tauri.conf.json")
+        .expect("Failed to read tauri.conf.json");
+  let config: serde_json::Value = serde_json::from_str(&config_str)
+        .expect("Failed to parse tauri.conf.json");
+
+  let version_str = config["package"]["version"].as_str()
+        .expect("Failed to get version from tauri.conf.json");
+
+  let version: String = version_str.chars().filter(|c| c.is_digit(10)).collect();
+
+  //ensure version is parsable as a number
+  let _version = version.parse::<u128>()
+        .expect(&format!{"Version {} is not a valid number", config_str});
+
+  //pass version as an environment variable
+  println!("cargo:rustc-env=VERSION={}", version);
+
+  //build
   tauri_build::build()
 }
