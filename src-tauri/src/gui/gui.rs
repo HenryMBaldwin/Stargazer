300c30ab62991cc26187c47416444e309e8fb64d
Henry Baldwin <henrymbaldwin@gmail.com>
2024-08-22 12:01:55 -0400

    some single instance code. Likely to be removed upon 2.0 migration.

--- src-tauri/src/gui/gui.rs
+++ src-tauri/src/gui/gui.rs
@@ -1,9 +1,35 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent, Manager, WindowEvent};
-
+use stargazer::libinstance::instance::{ClientInstance, generate_id};
+use chrono::Utc;
+use std::process::exit;
 mod pipe_client;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+  //client instance representing this client
+  let client = ClientInstance {
+    time: Utc::now(),
+    id: generate_id(),
+    version: env!("VERSION").to_string(),
+  };
+
+  //check if startup should continue
+  match pipe_client::phone_home().await {
+    Ok(response) => {
+      if response {
+        //no worries theres clearly already another client
+        exit(0)
+      }
+    }
+    Err(e) => {
+      println!("Error contemplating!");
+      exit(1)
+    }
+  }
+
+
+
   // Set up system tray menu
   let quit = CustomMenuItem::new("quit".to_string(), "Quit".to_string());
   let show = CustomMenuItem::new("show".to_string(), "Show".to_string());
@@ -16,6 +42,7 @@ fn main() {
     .with_menu(tray_menu);
 
   tauri::Builder::default()
+    .manage(client)
     .setup(|app| {
       let main_window = app.get_window("main").unwrap();
       let main_window_clone = main_window.clone(); // Clone the window handle
