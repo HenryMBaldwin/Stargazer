718c104451f77ab9508c224dc02ae7116cd863a7
Henry Baldwin <henrymbaldwin@gmail.com>
2024-09-05 19:16:22 -0400

    Added BUFFER_SIZE const for pipe buffer size.

--- src-tauri/src/libpipe/consts.rs
+++ src-tauri/src/libpipe/consts.rs
@@ -1,3 +1,4 @@
 //define useful constants to be shared between daemon and gui
 pub const PIPE_NAME: &str = r"\\.\pipe\stargazer_pipe";
 pub const PIPE_NAME_SERVER: &str = r"\\.\stargazer_pipe";
+pub const BUFFER_SIZE: usize = 1024;
